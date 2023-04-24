82a5da1c57a4f0a0c1986dd2c5ecf29f6e4b4288
Kenzo douchet <kenzodouchet33@gmail.com>
2023-04-25 00:37:02 +0200

    pages errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root to: "players#new"
   resources :players, except: [:new]
+
+  match "/404", to: "errors#not_found", via: :all
+  match "/500", to: "errors#internal_server_error", via: :all
 end
